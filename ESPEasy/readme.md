9ac781e0bc34a871d5783df3b4e0164b247a3526
Christian Meinert <cmeinert@gmx.net>
2019-11-30 10:00:57 +0100

    updates

--- ESPEasy/readme.md
+++ ESPEasy/readme.md
@@ -14,3 +14,21 @@ Save
 WifiConnect
 ```
 with the command `ip` you can get the ip-address of your ESP8266 after wifi connect.
+
+## log output
+
+when everything is configured correctly you should see this in your log
+
+```
+5355 : EVENT: MQTT#Connected
+5468 : C014 : autodiscover information of 4 Devices and 6 Nodes sent with no errors! (36 messages)
+5476 : P086 : broadcast topic: homie/$broadcast/testBroadcast subscribed.
+5480 : P086 : subscribe topic: test/homie subscribed.
+```
+
+
+- `C014 : autodiscover information of 4 Devices and 6 Nodes sent with no errors! (36 messages)?` shows you that the controller plugin initalized and send all collected information to the broker
+- `P086 : broadcast topic: homie/$broadcast/testBroadcast subscribed.?` if you use P086 (Homie receiver) with a broadcast channel you should see the toppic here
+- `P086 : subscribe topic: test/homie subscribed.` you can use P086 instead of the MQTT-receiver plugin and subscribe to any topic and receive **float** values
+
+
--- Node-RED/dashboard/ui-table/formatters.js
+++ Node-RED/dashboard/ui-table/formatters.js
@@ -1,9 +1,11 @@
 // formatters for ui-table
 
 // groupHeader (color & amount & plurals)
+var groupHeader = function (value, count, data, group) {
+    return value + "<span style='color:#d00; margin-left:10px;'>(" + count + " Thermostat"+((count>1) ? "e" : "") + "</span>";
+}
 
-var groupHeader = function (value, count, data, group) {return value + "<span style='color:#d00; margin-left:10px;'>(" + count + " Thermostat"+((count>1) ? "e" : "") + "</span>";}
-
+// groupHeader (color an alt text for 0 value)
 var groupHeader = function (value) {
     if (value>0)
         return "<span style='color:#ffffff;'>"+value+" min</span>";
@@ -11,6 +13,20 @@ var groupHeader = function (value) {
         return "<span style='color:#505050;'>aus</span>";
 }
 
+// add a unit
 var function(cell, formatterParams, onRendered){
     return cell.getValue()+"°C";
+}
+
+// convert Number to Icons
+var function(cell, formatterParams, onRendered){
+    var html="<i class=\"";
+    switch(cell.getValue()) {
+        case 0: html+="fa fa-calendar-check-o"; break;
+        case 1: html+="fa fa-hand-o-up"; break;
+        case 2: html+="fa fa-suitcase"; break;
+        case 3: html+="fa fa-spinner fa-spin fa-fw"; break;
+    }
+    html+='\"></i>';
+    return html;
 }
